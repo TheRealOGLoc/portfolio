2ff4768e6acc942d5dfc9206b87a7309813713c6
Yuchen Liu <lyc990312@hotmail.com>
2024-04-30 15:14:13 +1000

    set the basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import './App.scss';
+import Navbar from './component/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+     <section>
+      <Navbar/>
+     </section>
+     <section>Parallax</section>
+     <section>Services</section>
+     <section>Parallax</section>
+     <section>Portfolio</section>
+     <section>Portfolio</section>
+     <section>Portfolio</section>
+     <section>Contact</section>
     </div>
   );
 }
