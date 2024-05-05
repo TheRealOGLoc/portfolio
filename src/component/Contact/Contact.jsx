f00401fc8f756be9ffe8d6eaa14cab0b56da4d50
Yuchen Liu <lyc990312@hotmail.com>
2024-05-05 19:23:29 +1000

    Implement the send email feature

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./Contact.css"
 import { motion } from 'framer-motion'
+import emailjs from "@emailjs/browser"
 
 export default function Contact() {
+
+    const formRef = useRef()
+    const [error, setError] = useState(false)
+    const [success, setSuccess] = useState(false)
 
     const variants = {
         initial: {
@@ -19,31 +24,52 @@ export default function Contact() {
         }
     }
 
-  return (
-    <motion.div variants={variants} initial="initial" whileInView="animate" className='contact h-[100%] max-w-[1366px] m-auto flex items-center justify-center gap-[50px]'>
-        <motion.div variants={variants} className="contact-text-container flex-1 flex flex-col gap-[40px]">
-            <motion.h1 variants={variants} className='text-[60px] leading-[66px]'>Let's work together</motion.h1>
-            <motion.div variants={variants} className="contact-text-items">
-                <h2>Mail</h2>
-                <span>lyc990312@hotmail.com</span>
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_4t0h6ak', 'template_flj41g9', formRef.current, {
+                publicKey: 'HsvKZFr1WQEi5KoIe',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    setSuccess(true)
+                },
+                (error) => {
+                    setError(true)
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
+    return (
+        <motion.div variants={variants} initial="initial" whileInView="animate" className='contact h-[100%] max-w-[1366px] m-auto flex items-center justify-center gap-[50px]'>
+            <motion.div variants={variants} className="contact-text-container flex-1 flex flex-col gap-[40px]">
+                <motion.h1 variants={variants} className='text-[60px] leading-[66px]'>Let's work together</motion.h1>
+                <motion.div variants={variants} className="contact-text-items">
+                    <h2>Mail</h2>
+                    <span>lyc990312@hotmail.com</span>
+                </motion.div>
+                <motion.div variants={variants} className="contact-text-items">
+                    <h2>Address</h2>
+                    <span>Brisbane, Australia</span>
+                </motion.div>
+                <motion.div variants={variants} className="contact-text-items">
+                    <h2>Phone</h2>
+                    <span>+61 0426670312</span>
+                </motion.div>
             </motion.div>
-            <motion.div variants={variants}  className="contact-text-items">
-                <h2>Address</h2>
-                <span>Brisbane, Australia</span>
-            </motion.div>
-            <motion.div variants={variants} className="contact-text-items">
-                <h2>Phone</h2>
-                <span>+61 0426670312</span>
-            </motion.div>
+            <div className="flex-1">
+                <form onSubmit={sendEmail} ref={formRef} className='contact-form-container flex flex-col gap-[20px]'>
+                    <input name='name' type="text" placeholder='Name' />
+                    <input name='email' type="text" placeholder='E-mail' />
+                    <textarea name='message' rows={8} placeholder='Message'></textarea>
+                    <button className='p-[20px] bg-orange-500 '>Submit</button>
+                    {error && "Error"}
+                    {success && "Success"}
+                </form>
+            </div>
         </motion.div>
-        <div className="flex-1">
-            <form className='contact-form-container flex flex-col gap-[20px]'>
-                <input type="text" placeholder='Name' />
-                <input type="text" placeholder='E-mail' />
-                <textarea rows={8} placeholder='Message'></textarea>
-                <button className='p-[20px] bg-orange-500 '>Submit</button>
-            </form>
-        </div>
-    </motion.div>
-  )
+    )
 }
