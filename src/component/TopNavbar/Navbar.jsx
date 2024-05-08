9b3ea224989b42a3e84f277ac9dde56d7f8ba118
Yuchen Liu <lyc990312@hotmail.com>
2024-05-08 20:52:01 +1000

    Update the links to my social media

--- src/component/TopNavbar/Navbar.jsx
+++ src/component/TopNavbar/Navbar.jsx
@@ -8,12 +8,12 @@ export default function Navbar() {
     <div className='navbar h-[100px]'>
       <Sidebar/>
       <div className="navbar-wrapper max-w-[1366px] m-auto flex items-center justify-between h-[100%]">
-        <motion.span className="navbar-span font-bold pl-[150px]" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.3 }} >Yuchen DEV</motion.span>
+        <motion.span className="navbar-span font-bold pl-[150px]" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.3 }} ></motion.span>
         <div className='social flex gap-[10px]'>
-          <a href="#"><img src="/images/facebook.png" alt="" /></a>
-          <a href="#"><img src="/images/github.png" alt="" /></a>
-          <a href="#"><img src="/images/instagram.png" alt="" /></a>
-          <a href="#"><img src="/images/linkedin.png" alt="" /></a>
+          <a href="https://www.facebook.com/yuchen.liu.90663/"><img src="/images/facebook.png" alt="" /></a>
+          <a href="https://github.com/TheRealOGLoc"><img src="/images/github.png" alt="" /></a>
+          <a href="https://www.instagram.com/jojoho_kkk/"><img src="/images/instagram.png" alt="" /></a>
+          <a href="https://www.linkedin.com/in/yuchen-liu-se/"><img src="/images/linkedin.png" alt="" /></a>
         </div>
       </div>
     </div>
